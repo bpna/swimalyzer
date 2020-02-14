88bf6d73ff65db6e97c18d5f6ec24a7de588a775
Patrick Kinsella <pkinsella1@gmail.com>
2020-02-14 14:17:42 -0700

    add get_events(), get_result() to MeetResults class. Document and debug

--- results_manager.py
+++ results_manager.py
@@ -21,6 +21,40 @@ class MeetResults():
         self.womens_events = {}
         self.swimmers      = {}
 
+    def __swimmer_exists(self, swimmer):
+        """Raises an exception if the given swimmer did not participate in
+        this meet.
+
+        Args:
+            swimmer (str): a string that is the swimmer's name
+
+        Raises:
+            NameError: No such swimmer in this meet
+        """
+        if swimmer not in self.swimmers:
+            raise NameError('No such swimmer in this meet')
+
+    def __select_events(self, gender):
+        """Given a gender, returns the events dictionary for that gender.
+
+        Args:
+            gender (str): a string representing the swimmer's gender
+
+        Raises:
+            KeyError: 'Men' or 'Women' (case sensitive) must be supplied
+
+        Returns:
+            events: a reference to the events dictionary for the
+                    corresponding gender
+        """
+        switch = {
+            'Men':   self.mens_events,
+            'Women': self.womens_events
+        }
+        events = switch[gender]
+
+        return events
+
     def report_result(self, gender, result):
         """Takes a gender and an event result and adds the swimmer and time to
         the events dictionary for the corresponding gender and, if the swimmer
@@ -38,32 +72,71 @@ class MeetResults():
         Reporting a result for a swimmer of the same name in the same event
         raises a NameError exception.
         """
-        switch = {
-            'Men':   self.mens_events,
-            'Women': self.womens_events
-        }
-        events = switch[gender]
+        events = self.__select_events(gender)
 
         # Add result data to events dictionary for corresponding gender
-        if (result['event'] not in events):
+        if result['event'] not in events:
             events[result['event']] = {}
             events[result['event']][result['swimmer']] = result['time']
         else:
-            if (result['swimmer'] in events[result['event']]):
+            if result['swimmer'] in events[result['event']]:
                 raise NameError('Duplicate swimmer added to event')
             else:
                 events[result['event']][result['swimmer']] = result['time']
 
         # add swimmer data to swimmers dictionary
-        if (result['swimmer'] not in self.swimmers):
+        if result['swimmer'] not in self.swimmers:
             new_entry = result.copy()
             del new_entry['time']
             event = new_entry['event']
             del new_entry['event']
             new_entry['events'] = []
             new_entry['events'].append(event)
+            new_entry['gender'] = gender
             self.swimmers[result['swimmer']] = new_entry
         else:
             self.swimmers[result['swimmer']]['events'].append(result['event'])
 
-#    def get_result(self, event, swimmer)
+    def get_events(self, swimmer):
+        """Given a swimmer name, returns a list of events they were in.
+
+        Args:
+            swimmer (str): a string that is the swimmer's name
+
+        Raises:
+            NameError: No such swimmer in this meet
+
+        Returns:
+            events: a string list of events the swimmer participated in.
+        """
+        self.__swimmer_exists(swimmer)
+
+        return self.swimmers[swimmer]['events']
+
+    def get_result(self, swimmer, event):
+        """Given a swimmer name and event name, returns a timedelta object
+        representing their time in that event.
+
+        Args:
+            swimmer (str): a string that is the swimmer's name
+            event (str)  : a string that is the event name
+
+        Raises:
+            NameError: No such swimmer in this meet
+            NameError: No such event
+            NameError: No such swimmer in this event
+
+        Returns:
+            time: a timedelta object representing the swimmer's time in
+                  the event
+        """
+        self.__swimmer_exists(swimmer)
+        events = self.__select_events(self.swimmers[swimmer]['gender'])
+        if event not in events:
+            raise NameError('No such event')
+        else:
+            results = events[event]
+            if swimmer not in results:
+                raise NameError('No such swimmer in this event')
+            else:
+                return results[swimmer]
